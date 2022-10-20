9b55d4f5837d008e6621359deaa5d3728110fc15
Abdullah Al Tareq <113288325+AbdullahAlTareq@users.noreply.github.com>
2022-10-20 09:53:21 +0600

    Update ProjectSQL.sql

--- ProjectSQL.sql
+++ ProjectSQL.sql
@@ -1,3 +1,6 @@
+--1. 
+
+
 SELECT *
 FROM Covid_Deaths
 where continent is not null
@@ -11,14 +14,18 @@ SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM Covid_Deaths
 ORDER BY 1,2
 
---Looking at Total Cases vs Total Deaths
+----------------------------------------------------------------------------------------------------------------------------
+
+--2. Looking at Total Cases vs Total Deaths
 
 SELECT location, date, total_cases, total_deaths,(total_deaths/total_cases)*100 AS Death_rate
 FROM Covid_Deaths
 --WHERE location like '%bangladesh%'
 ORDER BY 1,2
 
---Looking at Total Cases vs Population
+-----------------------------------------------------------------------------------------------------------------------------
+
+--3. Looking at Total Cases vs Population
 
 SELECT location, date, population, total_deaths,(total_deaths/population)*100 AS Death_rate
 FROM Covid_Deaths
@@ -26,7 +33,10 @@ FROM Covid_Deaths
 ORDER BY 1,2
 
 
---Looking at Countries with Highest Infection Rate vs Population
+-----------------------------------------------------------------------------------------------------------------------------
+
+
+--4. Looking at Countries with Highest Infection Rate vs Population
 
 SELECT location, population, MAX(total_cases) AS highest_infection, MAX((total_cases/population))*100 AS infection_rate
 FROM Covid_Deaths
@@ -34,8 +44,10 @@ FROM Covid_Deaths
 GROUP BY location, population
 ORDER BY infection_rate DESC
 
+----------------------------------------------------------------------------------------------------------------------------
 
---Looking at Countries with Highest Death
+
+--5. Looking at Countries with Highest Death
 
 SELECT location, MAX(CAST(total_deaths AS INT)) AS highest_death
 FROM Covid_Deaths
@@ -52,8 +64,10 @@ where continent is null
 GROUP BY location
 ORDER BY highest_death DESC
 
+----------------------------------------------------------------------------------------------------------------------------
 
---Continent with Highest Death
+
+--6. Continent with Highest Death
 
 SELECT continent, MAX(CAST(total_deaths AS INT)) AS highest_death
 FROM Covid_Deaths
@@ -61,7 +75,10 @@ where continent is not null
 GROUP BY continent
 ORDER BY highest_death DESC
 
---Death Rate per Date
+---------------------------------------------------------------------------------------------------------------------------
+
+
+--7. Death Rate per Date
 
 SELECT date, SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS Death_rate
 FROM Covid_Deaths
@@ -70,15 +87,20 @@ GROUP BY date
 ORDER BY 1,2
 
 
---Global Number
+---------------------------------------------------------------------------------------------------------------------------
+
+
+--8. Global Number
 
 SELECT SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS Death_rate
 FROM Covid_Deaths
 where continent is not null
 ORDER BY 1,2
 
+---------------------------------------------------------------------------------------------------------------------------
 
---Total People vs Vaccination
+
+--9. Total People vs Vaccination
 
 
 select d.continent, d.location, d.date, d.population, v.new_vaccinations, sum(cast(v.new_vaccinations as int)) over (partition by d.location order by d.date) as rollingpeoplevaccicnated
@@ -89,9 +111,11 @@ and d.date = v.date
 WHERE d.continent is not null
 order by 2,3
 
+----------------------------------------------------------------------------------------------------------------------------
 
 
---Using Common Table Expression (CTE)
+
+--10.Using Common Table Expression (CTE)
 
 with populationVSvaccination(continent, location, date, population, new_vaccinations, rollingpeoplevaccicnated)
 as(
@@ -106,9 +130,11 @@ WHERE d.continent is not null
 select * , (rollingpeoplevaccicnated/population)*100
 from populationVSvaccination
 
+-------------------------------------------------------------------------------------------------------------------------
 
 
---TEMP Table
+
+--11. TEMP Table
 
 DROP TABLE if exists #percentpopulationvaccinated
 create table #percentpopulationvaccinated
@@ -133,9 +159,10 @@ select * , (rollingpeoplevaccicnated/population)*100 AS rollingpeoplevaccinated_
 from #percentpopulationvaccinated
 
 
+----------------------------------------------------------------------------------------------------------------------------
 
 
---Creating View for Visualisation
+--12. Creating View for Visualisation
 
 create view percentpopulationvaccinated as
 select d.continent, d.location, d.date, d.population, v.new_vaccinations, sum(cast(v.new_vaccinations as int)) over (partition by d.location order by d.date) as rollingpeoplevaccicnated
@@ -148,3 +175,6 @@ WHERE d.continent is not null
 
 select *
 from percentpopulationvaccinated
+
+
+-------------------------------------------------------------------That's all, Thank You----------------------------------------------
